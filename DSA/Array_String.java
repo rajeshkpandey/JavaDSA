ea8138696639b7283cd09948ad10e354dad0b364
Rajesh K Pandey <rajeshkpandey008@gmail.com>
2022-08-10 16:38:41 +0530

    Update Array_String.java

--- DSA/Array_String.java
+++ DSA/Array_String.java
@@ -1,4 +1,5 @@
 1.
+  
   boolean isUniqueChars(String str) { 
   if (str.length() > 128) return false; 
   boolean [] hash_at = new boolean[128];
@@ -25,4 +26,29 @@
 
 3.
   
-  
+  void replaceSpaces(char(J str, int trueLength) {
+    int spacecount,index,i;
+    for(i=0; i<str.size(); i++){
+      if(str[i] == ' ')
+        spacecount+=1;
+    }
+    index=trueLength+spacecount*3;
+    if(trueLength<str.size()) str[trueLength] = '\0';
+    for(i=trueLength-1;i>=0; i--){
+      if(str[i] == ' '){
+        str[index-1]='0';
+        str[index-2]='2';
+        str[index-3]='%';
+        index=index-3;
+      }
+      else{
+        str[index-1]=str[i];
+        index--;
+      }
+    }
+  }
+                     
+
+4.
+    
+   
